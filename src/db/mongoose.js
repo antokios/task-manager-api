bdaef5360a4ec08e1a6c9e473c97a3ca5136cedf
kiosses <kiosses@softweb.gr>
2021-04-29 13:44:15 +0300

    changed db connection method

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const chalk = require('chalk')
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}).then(() => {
-    console.log('Database connection established')
-}).catch((error) => {
-    console.log('Unable to connect to Database', error)
-})
+const connectDB = function () {
+    mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    }).then((res) => {
+        console.log(`${chalk.green.bold('Connection established')}. Database: ${chalk.green(`${res.connection.name}`)} Host: ${chalk.green(`${res.connection.host}`)} Port: ${chalk.green(`${res.connection.port}`)}`)
+    }).catch((e) => {
+        console.log('Unable to connect to Database', e)
+    })
+}
+
+module.exports = connectDB
